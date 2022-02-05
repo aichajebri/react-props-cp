8d9bb050deb25f6bad772ef808a330c864b19b81
aichajebri <77939143+aichajebri@users.noreply.github.com>
2022-02-05 15:20:28 +0000

    ajouter 9atous Aisha

--- src/profile/myData.js
+++ src/profile/myData.js
@@ -1,7 +1,7 @@
 import gojo from "./gojo.jpg";
 import "./style.css";
 const myData = {
-  fullName: "Gojo 9atous",
+  fullName: "Gojo 9atous Aisha",
   bio: "Owners of dogs will have noticed that, if you provide them with food and water and shelter and affection, they will think you are God. Whereas owners of cats are compelled to realize that, if you provide them with food and water and affection, they draw the conclusion that they are God.",
   profession: "A magnificient lil catto",
   img: gojo,
