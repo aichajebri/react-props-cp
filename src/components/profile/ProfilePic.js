41d8fb3934ec35f71d214bcb068b89ea928d3b1d
aichajebri <77939143+aichajebri@users.noreply.github.com>
2022-02-12 14:13:14 +0000

    update

--- src/components/profile/ProfilePic.js
+++ src/components/profile/ProfilePic.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './style.css';
-import photo from "C:/Users/pc/Desktop/components/checkpoint/src/components/profile/photo.png"
+import photo from "./photo.png";
 const profilePhoto =()=> {
     return <div className="img">
     <img src={photo} alt="Person"/>
